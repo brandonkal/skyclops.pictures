73985c53229b2e99adf0ce05eeda56a6ad40e9bd
popwebz <bwebservice@gmail.com>
2018-04-13 16:56:04 -0700

    Play background video on mobile

--- static/bgvideo/js/custom-vid.js
+++ static/bgvideo/js/custom-vid.js
@@ -6,28 +6,30 @@
      var is_mobile = false;
 
     if( $('.player').css('display')=='none') {
-        is_mobile = true;       
+        is_mobile = true;
     }
     if (is_mobile == true) {
         //Conditional script here
         $('.big-background, .small-background-section').addClass('big-background-default-image');
     }else{
-        $(".player").mb_YTPlayer(); 
+        $(".player").mb_YTPlayer();
     }
 
     });
 
 */
-    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
+    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT
 
         //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
         if (!device.tablet() && !device.mobile()) {
             $(".player").mb_YTPlayer();
         } else {
-            //jQuery will add the default background to the preferred class 
+            //jQuery will add the default background to the preferred class
             $('.big-background, .small-background-section').addClass(
                 'big-background-default-image');
         }
+        */
+        $(".player").mb_YTPlayer();
     });
 
 
